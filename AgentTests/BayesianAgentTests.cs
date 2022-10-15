c1cca5652f39f652a14308dc83c9faeff6b86918
Max <max.ledlie@spartansolutions.com>
2022-10-15 16:02:38 +0100

    Fixed tests. Option trackers now only store the number of each card remaining, not the probability of each one.

--- AgentTests/BayesianAgentTests.cs
+++ AgentTests/BayesianAgentTests.cs
@@ -34,7 +34,7 @@ namespace AgentTests
         }
 
         [Test]
-        public void HandProbabilities_GameStart_IsBasedOnlyOnTeammatesHands()
+        public void HandOptionTrackers_GameStart_IsBasedOnlyOnTeammatesHands()
         {
             var game = new Game(3, GameTests.TestDeck());
             var view = new GameView(0, game);
@@ -43,25 +43,25 @@ namespace AgentTests
             // There are 50 cards in total, so by default, each card has a (numInstances / 50) chance of
             // being any specific (color, number) pair. However, at the start of the game we can already
             // adjust our probabilities based on our teammates hands.
-            List<OptionTracker> probs = agent.HandOptionTrackers;
+            List<OptionTracker> optionTrackers = agent.HandOptionTrackers;
 
             for (int i = 0; i < 5; i++)
             {
-                Assert.That(probs[i].Get(new Card(Color.Red, 1)), Is.EqualTo(2.0 / 40));
-                Assert.That(probs[i].Get(new Card(Color.Red, 2)), Is.EqualTo(2.0 / 40));
-                Assert.That(probs[i].Get(new Card(Color.Red, 3)), Is.EqualTo(2.0 / 40));
-                Assert.That(probs[i].Get(new Card(Color.Red, 4)), Is.EqualTo(1.0 / 40));
-                Assert.That(probs[i].Get(new Card(Color.Red, 5)), Is.Zero);
-                Assert.That(probs[i].Get(new Card(Color.White, 1)), Is.EqualTo(3.0 / 40));
-                Assert.That(probs[i].Get(new Card(Color.White, 2)), Is.EqualTo(1.0 / 40));
-                Assert.That(probs[i].Get(new Card(Color.White, 3)), Is.EqualTo(2.0 / 40));
-                Assert.That(probs[i].Get(new Card(Color.White, 4)), Is.EqualTo(1.0 / 40));
-                Assert.That(probs[i].Get(new Card(Color.White, 5)), Is.Zero);
+                Assert.That(optionTrackers[i].Get(new Card(Color.Red, 1)), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(new Card(Color.Red, 2)), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(new Card(Color.Red, 3)), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(new Card(Color.Red, 4)), Is.EqualTo(1));
+                Assert.That(optionTrackers[i].Get(new Card(Color.Red, 5)), Is.Zero);
+                Assert.That(optionTrackers[i].Get(new Card(Color.White, 1)), Is.EqualTo(3));
+                Assert.That(optionTrackers[i].Get(new Card(Color.White, 2)), Is.EqualTo(1));
+                Assert.That(optionTrackers[i].Get(new Card(Color.White, 3)), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(new Card(Color.White, 4)), Is.EqualTo(1));
+                Assert.That(optionTrackers[i].Get(new Card(Color.White, 5)), Is.Zero);
             }
         }
 
         [Test]
-        public void HandProbabilities_AfterTell_ReflectsNewKnowledge()
+        public void HandOptionTrackers_AfterTell_ReflectsNewKnowledge()
         {
             var game = new Game(3, GameTests.TestDeck());
             var view = new GameView(0, game);
@@ -72,24 +72,24 @@ namespace AgentTests
             game.TellColor(1, Color.White); // Our agent tells player 1 something, just to progress the game
             game.TellColor(0, Color.Green); // Player 1 tells our agent about their green cards. This should update their knowledge.
 
-            var probs = agent.HandOptionTrackers;
+            var optionTrackers = agent.HandOptionTrackers;
 
             // We now know that our zeroth card is NOT green. Since we can see one green in our teammates hands,
             // this removes 9 possibilites, leaving us with 31
-            Assert.That(probs[0].Get(Color.Green, 1), Is.Zero);
-            Assert.That(probs[0].Get(Color.Red, 1), Is.EqualTo(2.0 / 31));
+            Assert.That(optionTrackers[0].Get(Color.Green, 1), Is.Zero);
+            Assert.That(optionTrackers[0].Get(Color.Red, 1), Is.EqualTo(2));
 
             // We now know that our first card IS green.
-            Assert.That(probs[1].Get(Color.Red, 1), Is.Zero);
-            Assert.That(probs[1].Get(Color.Green, 1), Is.EqualTo(2.0 / 9));
-            Assert.That(probs[1].Get(Color.Green, 2), Is.EqualTo(2.0 / 9));
-            Assert.That(probs[1].Get(Color.Green, 3), Is.EqualTo(2.0 / 9));
-            Assert.That(probs[1].Get(Color.Green, 4), Is.EqualTo(2.0 / 9));
-            Assert.That(probs[1].Get(Color.Green, 5), Is.EqualTo(1.0 / 9));
+            Assert.That(optionTrackers[1].Get(Color.Red, 1), Is.Zero);
+            Assert.That(optionTrackers[1].Get(Color.Green, 1), Is.EqualTo(2));
+            Assert.That(optionTrackers[1].Get(Color.Green, 2), Is.EqualTo(2));
+            Assert.That(optionTrackers[1].Get(Color.Green, 3), Is.EqualTo(2));
+            Assert.That(optionTrackers[1].Get(Color.Green, 4), Is.EqualTo(2));
+            Assert.That(optionTrackers[1].Get(Color.Green, 5), Is.EqualTo(1));
         }
 
         [Test]
-        public void HandProbabilities_AfterOwnDiscard_ReflectsNewKnowledge()
+        public void HandOptionTrackers_AfterOwnDiscard_ReflectsNewKnowledge()
         {
             var game = new Game(3, GameTests.TestDeck());
             var view = new GameView(0, game);
@@ -99,23 +99,23 @@ namespace AgentTests
 
             game.Discard(0);
 
-            var probs = agent.HandOptionTrackers;
+            var optionTrackers = agent.HandOptionTrackers;
 
             // Our agent has discarded a card and seen that it was a Red 1.
             // The probability of other cards in their hand being Red 1 should have decreased,
             // and the probability of them being any other card slightly increased.
             for (int i = 0; i < 5; i++)
             {
-                Assert.That(probs[i].Get(Color.Red, 1), Is.EqualTo(1.0 / 39));
-                Assert.That(probs[i].Get(Color.Red, 2), Is.EqualTo(2.0 / 39));
-                Assert.That(probs[i].Get(Color.Red, 3), Is.EqualTo(2.0 / 39));
-                Assert.That(probs[i].Get(Color.Red, 4), Is.EqualTo(1.0 / 39));
-                Assert.That(probs[i].Get(Color.Red, 5), Is.Zero);
+                Assert.That(optionTrackers[i].Get(Color.Red, 1), Is.EqualTo(1));
+                Assert.That(optionTrackers[i].Get(Color.Red, 2), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(Color.Red, 3), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(Color.Red, 4), Is.EqualTo(1));
+                Assert.That(optionTrackers[i].Get(Color.Red, 5), Is.Zero);
             }
         }
 
         [Test]
-        public void HandProbabilities_AfterTeammateDiscard_ReflectsNewKnowledge()
+        public void HandOptionTrackers_AfterTeammateDiscard_ReflectsNewKnowledge()
         {
             var game = new Game(3, GameTests.TestDeck());
             var view = new GameView(0, game);
@@ -126,25 +126,25 @@ namespace AgentTests
             game.TellNumber(1, 4); // Tell Player 1 about their number 4 just to skip our turn
             game.Discard(2); // Player 1 discards their 4, and it is replaced with the White 1 from the top of the deck.
 
-            var probs = agent.HandOptionTrackers;
+            var optionTrackers = agent.HandOptionTrackers;
 
             // Our agent should recognise that Player 1 now has a White 1. Their probability of any card in their hand being
             // a White 1 should be reduced, and the probability of them being any other card slightly increased.
             for (int i = 0; i < 5; i++)
             {
-                Assert.That(probs[i].Get(Color.White, 1), Is.EqualTo(2.0 / 39));
-                Assert.That(probs[i].Get(Color.White, 2), Is.EqualTo(1.0 / 39));
-                Assert.That(probs[i].Get(Color.White, 3), Is.EqualTo(2.0 / 39));
-                Assert.That(probs[i].Get(Color.White, 4), Is.EqualTo(1.0 / 39));
-                Assert.That(probs[i].Get(Color.White, 5), Is.Zero);
+                Assert.That(optionTrackers[i].Get(Color.White, 1), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(Color.White, 2), Is.EqualTo(1));
+                Assert.That(optionTrackers[i].Get(Color.White, 3), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(Color.White, 4), Is.EqualTo(1));
+                Assert.That(optionTrackers[i].Get(Color.White, 5), Is.Zero);
 
                 // They should remember that they saw a Red 4, even though it's now in the discard pile.
-                Assert.That(probs[i].Get(Color.Red, 4), Is.EqualTo(1.0 / 39)); 
+                Assert.That(optionTrackers[i].Get(Color.Red, 4), Is.EqualTo(1)); 
             }
         }
 
         [Test]
-        public void HandProbabilities_AfterOwnPlay_ReflectsNewKnowledge()
+        public void HandOptionTrackers_AfterOwnPlay_ReflectsNewKnowledge()
         {
             var game = new Game(3, GameTests.TestDeck());
             var view = new GameView(0, game);
@@ -154,23 +154,23 @@ namespace AgentTests
 
             game.PlayCard(0);
 
-            var probs = agent.HandOptionTrackers;
+            var optionTrackers = agent.HandOptionTrackers;
 
             // Our agent has played a card and seen that it was a Red 1.
             // The probability of other cards in their hand being Red 1 should have decreased,
             // and the probability of them being any other card slightly increased.
             for (int i = 0; i < 5; i++)
             {
-                Assert.That(probs[i].Get(Color.Red, 1), Is.EqualTo(1.0 / 39));
-                Assert.That(probs[i].Get(Color.Red, 2), Is.EqualTo(2.0 / 39));
-                Assert.That(probs[i].Get(Color.Red, 3), Is.EqualTo(2.0 / 39));
-                Assert.That(probs[i].Get(Color.Red, 4), Is.EqualTo(1.0 / 39));
-                Assert.That(probs[i].Get(Color.Red, 5), Is.Zero);
+                Assert.That(optionTrackers[i].Get(Color.Red, 1), Is.EqualTo(1));
+                Assert.That(optionTrackers[i].Get(Color.Red, 2), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(Color.Red, 3), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(Color.Red, 4), Is.EqualTo(1));
+                Assert.That(optionTrackers[i].Get(Color.Red, 5), Is.Zero);
             }
         }
 
         [Test]
-        public void HandProbabilities_AfterTeammatePlay_ReflectsNewKnowledge()
+        public void HandOptionTrackers_AfterTeammatePlay_ReflectsNewKnowledge()
         {
             var game = new Game(3, GameTests.TestDeck());
             var view = new GameView(0, game);
@@ -181,20 +181,20 @@ namespace AgentTests
             game.TellNumber(1, 4); // Tell Player 1 about their number 4 just to skip our turn
             game.PlayCard(2); // Player 1 plays their 4 (losing a life), and it is replaced with the White 1 from the top of the deck.
 
-            var probs = agent.HandOptionTrackers;
+            var optionTrackers = agent.HandOptionTrackers;
 
             // Our agent should recognise that Player 1 now has a White 1. Their probability of any card in their hand being
             // a White 1 should be reduced, and the probability of them being any other card slightly increased.
             for (int i = 0; i < 5; i++)
             {
-                Assert.That(probs[i].Get(Color.White, 1), Is.EqualTo(2.0 / 39));
-                Assert.That(probs[i].Get(Color.White, 2), Is.EqualTo(1.0 / 39));
-                Assert.That(probs[i].Get(Color.White, 3), Is.EqualTo(2.0 / 39));
-                Assert.That(probs[i].Get(Color.White, 4), Is.EqualTo(1.0 / 39));
-                Assert.That(probs[i].Get(Color.White, 5), Is.Zero);
+                Assert.That(optionTrackers[i].Get(Color.White, 1), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(Color.White, 2), Is.EqualTo(1));
+                Assert.That(optionTrackers[i].Get(Color.White, 3), Is.EqualTo(2));
+                Assert.That(optionTrackers[i].Get(Color.White, 4), Is.EqualTo(1));
+                Assert.That(optionTrackers[i].Get(Color.White, 5), Is.Zero);
 
                 // They should remember that they saw a Red 4, even though it's now in the discard pile.
-                Assert.That(probs[i].Get(Color.Red, 4), Is.EqualTo(1.0 / 39));
+                Assert.That(optionTrackers[i].Get(Color.Red, 4), Is.EqualTo(1));
             }
         }
     }
